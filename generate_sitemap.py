9b8c4bd716ce5a367d6e2163d639e7094bea5975
Dale Poulter <dalepoulter@gmail.com>
2023-07-05 17:57:43 +0100

    Update generate_sitemap.py
    Added exception handlers around parse and request operations so that invalid urls are ignored and the program will continue processing

--- generate_sitemap.py
+++ generate_sitemap.py
@@ -17,18 +17,25 @@ def crawl(url, base_url):
         response = requests.get(url)
         response.raise_for_status()  # raise exception if the request failed
         print(f"Successfully fetched {url}")
-    except Exception as e:
+    except requests.exceptions.RequestException as e:
         print(f"Failed to fetch {url}. Exception: {e}")
         return
-    soup = BeautifulSoup(response.text, 'html.parser')
-    for a_tag in soup.find_all('a'):
-        href = a_tag.get('href')
-        if href and not href.startswith('#') and not href.startswith('mailto:'):
-            full_url = urljoin(url, href)
-            if same_domain(full_url, base_url) and full_url not in visited:
-                print(f'Parsed URL: {full_url}')  # print parsed URL
-                yield full_url
-                yield from crawl(full_url, base_url)
+    except Exception as e:
+        print(f"An error occurred while fetching {url}. Exception: {e}")
+        return
+    try:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        for a_tag in soup.find_all('a'):
+            href = a_tag.get('href')
+            if href and not href.startswith('#') and not href.startswith('mailto:'):
+                full_url = urljoin(url, href)
+                if same_domain(full_url, base_url) and full_url not in visited:
+                    print(f'Parsed URL: {full_url}')  # print parsed URL
+                    yield full_url
+                    yield from crawl(full_url, base_url)
+    except Exception as e:
+        print(f"An error occurred while parsing {url}. Exception: {e}")
+        return
 
 def build_sitemap(start_url):
     print(f"Building sitemap for {start_url}")
@@ -49,14 +56,17 @@ def main():
     parser = argparse.ArgumentParser(description='Generate a sitemap for a website.')
     parser.add_argument('start_url', help='The URL of the website to generate a sitemap for.')
     args = parser.parse_args()
-    
+
     print(f"Starting sitemap generation for {args.start_url}")
-    sitemap = build_sitemap(args.start_url)
+    try:
+        sitemap = build_sitemap(args.start_url)
 
-    with open('sitemap.xml', 'wb') as f:
-        f.write(sitemap)
+        with open('sitemap.xml', 'wb') as f:
+            f.write(sitemap)
 
-    print("Finished sitemap generation")
+        print("Finished sitemap generation")
+    except Exception as e:
+        print(f"An error occurred during sitemap generation. Exception: {e}")
 
 if __name__ == '__main__':
     main()
